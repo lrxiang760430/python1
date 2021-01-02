9ef27a1e68f7d53fe6c140e3c1c5d311a373a7b9
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2021-01-02 11:23:19 +0800

    增加了一部分字符串的内容

--- 20210102.py
+++ 20210102.py
@@ -1,3 +1,15 @@
 # 2021年1月2日python练习
 print("7"+"1")
 print(7+1)
+result=77+66
+print(result)
+number1=77
+number2=66
+result=number1-number2
+print(result)
+result=16%3
+print(result)
+c='''I
+have a 
+dream'''
+print(c)
