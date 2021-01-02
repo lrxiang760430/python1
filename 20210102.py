f2e88b7e4ce757444c14142ccc2370d374ccc2f9
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2021-01-02 19:53:46 +0800

    增加部分字符串的练习

--- 20210102.py
+++ 20210102.py
@@ -13,3 +13,26 @@ c='''I
 have a 
 dream'''
 print(c)
+#name="Downey"
+#print(name)
+name="Downey"
+birthday="19650404"
+print(f"his name is {name}")
+print(f"his birthday is {birthday}")
+
+Date="2018年10月3日"
+Day="星期三"
+print(Date+Day)
+
+print("When you love somebody\nYou don't need proof\nYou can feel it")
+
+#City="Beijing"
+#print(City)
+
+City="Beijing"
+print(City+" is a great city\n"+"A lot of people live in "+ City+ "\n" +"Let's make "
+
+abc=100
+b=200
+print(abc<b)
+print(abc>b)
