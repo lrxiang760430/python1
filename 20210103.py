4a7a333d3d961436e9dbe239f3096aa37bfe60ce
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2021-01-03 18:41:26 +0800

    主要是判断语句的学习

---
+++ 20210103.py
@@ -0,0 +1,25 @@
+"""
+print("hello world!")
+Price=500
+Balance=600
+if(Price<Balance):
+    print("yes")
+if(Price>Balance):
+    print("no")
+"""
+
+MyHeight=171
+YourHeight=181
+if(MyHeight<YourHeight):
+    print("I'm shorter than you")
+else:
+    print("I am not shorter than you")
+
+
+
+n=786
+if(n%2==0):
+    print("n为偶数")
+else:
+    print("n为奇数")
+
