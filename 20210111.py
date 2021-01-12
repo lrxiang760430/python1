5dfcbcf02d36459cfcdb5580d08af9e98adb8e92
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2021-01-12 16:07:42 +0800

    重新学习了数据清洗
    其中去掉重复值那一项特别好用，用这个方法可以去掉radius产生的日志文件中的重复值，这样就可以把不重复的用户导出来了；
    读取txt文件，可以用read_table，注意后面用sep= ','来进行不同列的划分
    写入文件用to_csv

--- 20210111.py
+++ 20210111.py
@@ -1,6 +1,5 @@
-
 import pandas as pd 
-df=pd.read_csv(r'c:\tools\视频会员订单数据源.csv') 
+df=pd.read_csv(r'G:\tools\mathorcup\sphy.csv') 
 print (df)
 
 df["price"]=df["price"]/100
@@ -18,17 +17,39 @@ df.info()
 
 df['platform'].fillna("unknown",inplace=True)
 df.info()
+
+print(df[df["order_id"]<=0])
+print(df[df["user_id"]<=0])
+print(df)
+#print(df['price'].isin([25.00,68.00,248.00]))
+print(~df['price'].isin([25.00,68.00,248.00]))
+dfWrongPrice=df[~df['price'].isin([25.00,68.00,248.00])]
+print(dfWrongPrice)
+df.drop(index=dfWrongPrice.index,inplace=True)
+
+dfWrongPlat=df[~df['platform'].isin(["ios","android"])]
+print(dfWrongPlat)
+
+dfWrongPay=df[~df['payment_provider'].isin(["wxpay","alipay","applepay"])]
+print(dfWrongPay)
+
+dfWrongTime=df[df['pay_time']<df['create_time']]
+print(dfWrongTime)
+df.drop(index=dfWrongTime.index,inplace=True)
+
+
+df.info()
+
+print(df['order_id'].duplicated())
+dfOrderDu=df[df['order_id'].duplicated()]
+print(dfOrderDu)
+df.drop(index=dfOrderDu.index,inplace=True)
+df.info()
 """
 dfPayNull=df[df['payment_provider'].isnull()]
-
 df.drop(index=dfPayNull.index,inplace=True)
-
 df.info()
-
 dfPlatNull=df[df['platform'].isnull()]
-
 df.drop(index=dfPlatNull.index,inplace=True)
-
 df.info()
-
 """
