0a54205fac31d9c454761fedb1f915d6ae7d5bc1
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2021-01-22 18:07:38 +0800

    主要实现了从radius日志文件中把用户帐户和密码提取出来

---
+++ 20210122.py
@@ -0,0 +1,45 @@
+import pandas as pd 
+account=pd.read_csv(r'G:\tools\python1\account.csv')
+print(account)
+print(account.head())
+
+#去掉pppoe和mac列中的无关字符，只保留帐号和MAC地址
+#onuggl['MAC地址 / SN']=onuggl['MAC地址 / SN'].str.replace(":","")
+account['pppoe']=account['pppoe'].str.replace('net_no','')
+account['pppoe']=account['pppoe'].str.replace('[','')
+account['pppoe']=account['pppoe'].str.replace('{','')
+account['pppoe']=account['pppoe'].str.replace('"":"','')
+account['pppoe']=account['pppoe'].str.replace('"','')
+account['mac']=account['mac'].str.replace('calling_station_id:','')
+account['mac']=account['mac'].str.replace('"','')
+account['mac']=account['mac'].str.replace(':','')
+
+#"":"
+print(account.head())
+
+#下面进行去重操作
+account.info()
+
+print(account['pppoe'].duplicated()==True)
+
+#account=account['pppoe'].drop_duplicates()
+account=account.drop_duplicates(subset=['pppoe','mac'])
+print(account)
+
+account1=account[['pppoe','mac']]
+print(account1)
+
+#去掉pppoe和mac里面有空值的行
+account1=account1.dropna()
+print(account1)
+
+account1.to_csv('account20210122.csv')
+"""
+#[{"net_no":"8370810395886945@zc"  acct_terminate_cause:"2"   acct_output_octets:"329536152"  ...  nas_ip:"1.2.3.4"  calling_station_id:"40:16:9f:24:36:35"    acct_output_packets:"329849"}]
+
+#给包含用户帐户和MAC地址的这两列改列名
+account.rename(columns={'[{"net_no":"8370810395886945@zc"','pppoe'})
+
+#account.to_csv('account.csv')
+
+"""
