3d197a64cf160fda1b051f63149770e718e0a428
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2022-03-08 17:06:29 +0800

    重新学习了字符串方面的知识

---
+++ 20220308.py
@@ -0,0 +1,21 @@
+verdor1='Cisco'
+print(verdor1)
+poetry='''一片花飞减却春，风飘万点正愁人。
+传语风光共流转，暂时相赏莫相违。'''
+poetry
+print(poetry)
+vendor='Huawei'
+print(vendor.upper())
+print(vendor.lower())
+ip='     10.1.1.1    '
+print(ip.strip())
+ip1='######10.1.1.2##############'
+print(ip1.strip('#'))
+number='7393838998839999933'
+print(number.count('9'))
+#age=input('小朋友，你几岁了啊？')
+#print(age)
+print(ip.startswith('10'))
+a='10'
+print(a.isdigit())
+print(vendor.isalpha())
