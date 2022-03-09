47ca2673350fe1963ad730527d888d5f4896e209
lrxiang760430 <49584360+lrxiang760430@users.noreply.github.com>
2022-03-09 11:36:37 +0800

    布尔运算

---
+++ 20220309布尔运算.py
@@ -0,0 +1,22 @@
+print(1==1)
+print(1==2)
+print(1!=1)
+print(1!=2)
+a=1
+print(a==1)
+print(a==2)
+print(a<2)
+print(a<=2)
+print(a>=2)
+print(1==1 and 2==2)
+print(1==2 and 2==2)
+print(1==2 or 2==2)
+print(1==2 or 2==1)
+print(not (1==2 or 2==1))
+a=True
+print(not a)
+a='Huawei'
+print('H' in a)
+print(bool(' '))
+print(bool(''))
+print(bool(['',]))
