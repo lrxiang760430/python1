80191b7041266c21e448bec0d2d29133cce8a4a6
lrxiang760430 <lrxiang@163.com>
2023-02-25 06:47:03 +0800

    打印总行数，以及两个去重后的行数

---
+++ 2023sd4g.py
@@ -0,0 +1,30 @@
+'''
+
+import os
+#打印上级目录
+print(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+
+path1=os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+
+'''
+import pandas as pd
+
+df=pd.read_csv('d:/tools/sd/20230224sd4g.csv',encoding='gbk')
+print(df)
+
+
+#打印表头
+print(df.columns)
+#整行去重
+df=df.drop_duplicates()
+#查看整行去重后还有多少行
+print(df)
+#查看以'站号（E-NODEB ID）'去重还有多少行
+df=df.drop_duplicates('站号（E-NODEB ID）')
+print(df)
+#查看以'站名'去重还有多少行
+df=df.drop_duplicates('站名')
+print(df)
+
