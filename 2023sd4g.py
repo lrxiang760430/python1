49c8c67916b96862f2ca59f2c6f34d2c8951c0fd
lrxiang760430 <lrxiang@163.com>
2023-02-25 10:35:33 +0800

    增加了以经纬度去重的功能

--- 2023sd4g.py
+++ 2023sd4g.py
@@ -28,7 +28,21 @@ df=df.drop_duplicates('站号（E-NODEB ID）')
 
 
 print(df)
+print('以上是以站号（E-NODEB ID）去重')
 #查看以'站名'去重还有多少行
 df=df.drop_duplicates('站名')
 print(df)
+print('以上是以站名去重')
 
+#下面开始以经纬度去重
+#首先完成经纬度的合并
+#这个是之前编写的代码
+#df['经纬度']=df['扇区经度'].map(str)+' '+df['扇区纬度'].map(str)
+df['经纬度']=df['经度'].map(str)+' '+df['纬度'].map(str)
+#打印
+print(df)
+print(df.columns)
+print('以上看看是否有合并后的经纬度了')
+df=df.drop_duplicates('经纬度')
+print(df)
+print('以上显示以经纬度去重后的数量')
