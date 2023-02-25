53bf7590f6fb11db4733ba6e878448d090d83d6a
lrxiang760430 <lrxiang@163.com>
2023-02-25 10:19:18 +0800

    2月25日在单位的笔记本上调试的，修改了csv文件的路径。

--- 2023sd4g.py
+++ 2023sd4g.py
@@ -11,7 +11,9 @@ path1=os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 '''
 import pandas as pd
 
-df=pd.read_csv('d:/tools/sd/20230224sd4g.csv',encoding='gbk')
+#df=pd.read_csv('d:/tools/sd/20230224sd4g.csv',encoding='gbk')
+#在笔记本电脑上打开的路径
+df=pd.read_csv('c:/tools/sd/202301山东.csv',encoding='gbk')
 print(df)
 
 
@@ -23,6 +25,8 @@ df=df.drop_duplicates()
 print(df)
 #查看以'站号（E-NODEB ID）'去重还有多少行
 df=df.drop_duplicates('站号（E-NODEB ID）')
+
+
 print(df)
 #查看以'站名'去重还有多少行
 df=df.drop_duplicates('站名')
