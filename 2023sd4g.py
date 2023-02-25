ed9302f6aba693a0dfdced560a342c5191d41fdf
lrxiang760430 <lrxiang@163.com>
2023-02-25 16:09:48 +0800

    使用了df1 df2 df3 df4来分别取df的值，避免了重复使用df取值带来的数据不准确

--- 2023sd4g.py
+++ 2023sd4g.py
@@ -20,18 +20,19 @@ print(df)
 #打印表头
 print(df.columns)
 #整行去重
-df=df.drop_duplicates()
+df1=df.drop_duplicates()
 #查看整行去重后还有多少行
-print(df)
+print(df1)
+print('以上是整行去重后得到的行数')
+
 #查看以'站号（E-NODEB ID）'去重还有多少行
-df=df.drop_duplicates('站号（E-NODEB ID）')
-
-
-print(df)
+df2=df.drop_duplicates('站号（E-NODEB ID）')
+print(df2)
 print('以上是以站号（E-NODEB ID）去重')
+
 #查看以'站名'去重还有多少行
-df=df.drop_duplicates('站名')
-print(df)
+df3=df.drop_duplicates('站名')
+print(df3)
 print('以上是以站名去重')
 
 #下面开始以经纬度去重
@@ -43,8 +44,8 @@ df['经纬度']=df['经度'].map(str)+' '+df['纬度'].map(str)
 print(df)
 print(df.columns)
 print('以上看看是否有合并后的经纬度了')
-df=df.drop_duplicates('经纬度')
-print(df)
+df4=df.drop_duplicates('经纬度')
+print(df4)
 print('以上显示以经纬度去重后的数量')
 #根据TAC去重 跟踪区码（TAC）
 df=df.drop_duplicates('跟踪区码（TAC）')
