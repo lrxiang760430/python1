d5e54315b43703a9af79f200ca2061d4ee9598d6
lrxiang760430 <lrxiang@163.com>
2023-02-27 12:00:29 +0800

    增加了生成相应csv文件的功能

--- 2023sd4g.py
+++ 2023sd4g.py
@@ -24,16 +24,20 @@ df1=df.drop_duplicates()
 #查看整行去重后还有多少行
 print(df1)
 print('以上是整行去重后得到的行数')
+df1.to_csv('c:/tools/sd/20230227/整行去重后得到的文件.csv')
 
 #查看以'站号（E-NODEB ID）'去重还有多少行
 df2=df.drop_duplicates('站号（E-NODEB ID）')
 print(df2)
 print('以上是以站号（E-NODEB ID）去重')
+df2.to_csv('c:/tools/sd/20230227/以站号（E-NODEB ID）去重.csv')
 
 #查看以'站名'去重还有多少行
 df3=df.drop_duplicates('站名')
 print(df3)
 print('以上是以站名去重')
+df3.to_csv('c:/tools/sd/20230227/以站名去重.csv')
+
 
 #下面开始以经纬度去重
 #首先完成经纬度的合并
@@ -47,9 +51,13 @@ print('以上看看是否有合并后的经纬度了')
 df4=df.drop_duplicates('经纬度')
 print(df4)
 print('以上显示以经纬度去重后的数量')
+df4.to_csv('c:/tools/sd/20230227/经纬度去重.csv')
+
 #根据TAC去重 跟踪区码（TAC）
-df=df.drop_duplicates('跟踪区码（TAC）')
-print(df)
+df5=df.drop_duplicates('跟踪区码（TAC）')
+print(df5)
 print('以上显示以跟踪区码（TAC）去重后的数量')
+df5.to_csv('c:/tools/sd/20230227/以跟踪区码（TAC）去重.csv')
+
 
 print('只是为了学习git的操作')
