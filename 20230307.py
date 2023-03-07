a5f62d5e4b506a217f6cc71210f04f59c8dba893
lrxiang760430 <lrxiang@163.com>
2023-03-07 11:20:02 +0800

    使用columns打印表头

--- 20230307.py
+++ 20230307.py
@@ -1,4 +1,11 @@
 import pandas as pd
+'''
 df=pd.read_excel('c:/tools/sd/工作簿2.xls')
 print(df)
 df.to_csv('c:/tools/sd/20230306chr.csv')
+'''
+#20230307.csv
+df=pd.read_csv('c:/tools/sd/20230307.csv')
+print(df)
+#打印表头
+print(df.columns)
