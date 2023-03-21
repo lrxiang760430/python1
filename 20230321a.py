a26d0fe07a25d648dbbf701baeb20024ca3981f6
lrxiang760430 <lrxiang@163.com>
2023-03-21 12:08:37 +0800

    用这个程序来学习python中的筛选

---
+++ 20230321a.py
@@ -0,0 +1,6 @@
+#可以测试用python处理chr数据
+import pandas as pd
+#使用,作为分隔符
+#df=pd.read_csv('C:/tools/sd/chr/20230321/chr20230321.csv',sep=',')
+df=pd.read_csv('C:/tools/sd/chr/20230321/1679297411403_call_s_cscf_5_0.csv')
+print(df)
